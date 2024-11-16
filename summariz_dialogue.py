06e5c30dc2b3b32aa13876aaf50547f89513d2a5
zganjei <zganjei@users.noreply.github.com>
2024-11-16 06:38:20 +0100

    Update summariz_dialogue.py

--- summariz_dialogue.py
+++ summariz_dialogue.py
@@ -1,1 +1,255 @@
+import os
 
+instance_type_expected = 'ml-m5-2xlarge'
+instance_type_current = os.environ.get('HOSTNAME')
+
+print(f'Expected instance type: instance-datascience-{instance_type_expected}')
+print(f'Currently chosen instance type: {instance_type_current}')
+
+assert instance_type_expected in instance_type_current, f'ERROR. You selected the {instance_type_current} instance type. Please select {instance_type_expected} instead as shown on the screenshot above'
+print("Instance type has been chosen correctly.")
+
+
+%pip install -U datasets==2.17.0
+
+%pip install --upgrade pip
+%pip install --disable-pip-version-check \
+    torch==1.13.1 \
+    torchdata==0.5.1 --quiet
+
+%pip install \
+    transformers==4.27.2 --quiet
+
+from datasets import load_dataset
+from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+from transformers import GenerationConfig
+
+huggingface_dataset_name = "knkarthick/dialogsum"
+
+dataset = load_dataset(huggingface_dataset_name)
+
+
+example_indices = [40, 200]
+
+dash_line = '-'.join('' for x in range(100))
+
+for i, index in enumerate(example_indices):
+    print(dash_line)
+    print('Example ', i + 1)
+    print(dash_line)
+    print('INPUT DIALOGUE:')
+    print(dataset['test'][index]['dialogue'])
+    print(dash_line)
+    print('BASELINE HUMAN SUMMARY:')
+    print(dataset['test'][index]['summary'])
+    print(dash_line)
+    print()
+
+
+model_name='google/flan-t5-base'
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+
+sentence = "What time is it, Tom?"
+
+sentence_encoded = tokenizer(sentence, return_tensors='pt')
+
+sentence_decoded = tokenizer.decode(
+        sentence_encoded["input_ids"][0], 
+        skip_special_tokens=True
+    )
+
+print('ENCODED SENTENCE:')
+print(sentence_encoded["input_ids"][0])
+print('\nDECODED SENTENCE:')
+print(sentence_decoded)
+
+
+for i, index in enumerate(example_indices):
+    dialogue = dataset['test'][index]['dialogue']
+    summary = dataset['test'][index]['summary']
+    
+    inputs = tokenizer(dialogue, return_tensors='pt')
+    output = tokenizer.decode(
+        model.generate(
+            inputs["input_ids"], 
+            max_new_tokens=50,
+        )[0], 
+        skip_special_tokens=True
+    )
+    
+    print(dash_line)
+    print('Example ', i + 1)
+    print(dash_line)
+    print(f'INPUT PROMPT:\n{dialogue}')
+    print(dash_line)
+    print(f'BASELINE HUMAN SUMMARY:\n{summary}')
+    print(dash_line)
+    print(f'MODEL GENERATION - WITHOUT PROMPT ENGINEERING:\n{output}\n')
+
+
+for i, index in enumerate(example_indices):
+    dialogue = dataset['test'][index]['dialogue']
+    summary = dataset['test'][index]['summary']
+
+    prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary:
+    """
+
+    # Input constructed prompt instead of the dialogue.
+    inputs = tokenizer(prompt, return_tensors='pt')
+    output = tokenizer.decode(
+        model.generate(
+            inputs["input_ids"], 
+            max_new_tokens=50,
+        )[0], 
+        skip_special_tokens=True
+    )
+    
+    print(dash_line)
+    print('Example ', i + 1)
+    print(dash_line)
+    print(f'INPUT PROMPT:\n{prompt}')
+    print(dash_line)
+    print(f'BASELINE HUMAN SUMMARY:\n{summary}')
+    print(dash_line)    
+    print(f'MODEL GENERATION - ZERO SHOT:\n{output}\n')
+
+
+
+for i, index in enumerate(example_indices):
+    dialogue = dataset['test'][index]['dialogue']
+    summary = dataset['test'][index]['summary']
+        
+    prompt = f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+"""
+
+    inputs = tokenizer(prompt, return_tensors='pt')
+    output = tokenizer.decode(
+        model.generate(
+            inputs["input_ids"], 
+            max_new_tokens=50,
+        )[0], 
+        skip_special_tokens=True
+    )
+
+    print(dash_line)
+    print('Example ', i + 1)
+    print(dash_line)
+    print(f'INPUT PROMPT:\n{prompt}')
+    print(dash_line)
+    print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+    print(dash_line)
+    print(f'MODEL GENERATION - ZERO SHOT:\n{output}\n')
+
+
+def make_prompt(example_indices_full, example_index_to_summarize):
+    prompt = ''
+    for index in example_indices_full:
+        dialogue = dataset['test'][index]['dialogue']
+        summary = dataset['test'][index]['summary']
+        
+        # The stop sequence '{summary}\n\n\n' is important for FLAN-T5. Other models may have their own preferred stop sequence.
+        prompt += f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+{summary}
+
+
+"""
+    
+    dialogue = dataset['test'][example_index_to_summarize]['dialogue']
+    
+    prompt += f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+"""
+        
+    return prompt
+
+
+example_indices_full = [40]
+example_index_to_summarize = 200
+
+one_shot_prompt = make_prompt(example_indices_full, example_index_to_summarize)
+
+print(one_shot_prompt)
+
+
+summary = dataset['test'][example_index_to_summarize]['summary']
+
+inputs = tokenizer(one_shot_prompt, return_tensors='pt')
+output = tokenizer.decode(
+    model.generate(
+        inputs["input_ids"],
+        max_new_tokens=50,
+    )[0], 
+    skip_special_tokens=True
+)
+
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+print(dash_line)
+print(f'MODEL GENERATION - ONE SHOT:\n{output}')
+
+example_indices_full = [73, 1, 100]
+example_index_to_summarize = 500
+
+few_shot_prompt = make_prompt(example_indices_full, example_index_to_summarize)
+
+print(few_shot_prompt)
+
+summary = dataset['test'][example_index_to_summarize]['summary']
+
+inputs = tokenizer(few_shot_prompt, return_tensors='pt')
+output = tokenizer.decode(
+    model.generate(
+        inputs["input_ids"],
+        max_new_tokens=50,
+    )[0], 
+    skip_special_tokens=True
+)
+
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+print(dash_line)
+print(f'MODEL GENERATION - FEW SHOT:\n{output}')
+
+# generation_config = GenerationConfig(max_new_tokens=50)
+# generation_config = GenerationConfig(max_new_tokens=10)
+# generation_config = GenerationConfig(max_new_tokens=50, do_sample=True, temperature=0.1)
+# generation_config = GenerationConfig(max_new_tokens=50, do_sample=True, temperature=0.5)
+generation_config = GenerationConfig(max_new_tokens=50, do_sample=True, temperature=1.0)
+
+inputs = tokenizer(few_shot_prompt, return_tensors='pt')
+output = tokenizer.decode(
+    model.generate(
+        inputs["input_ids"],
+        generation_config=generation_config,
+    )[0], 
+    skip_special_tokens=True
+)
+
+print(dash_line)
+print(f'MODEL GENERATION - FEW SHOT:\n{output}')
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
